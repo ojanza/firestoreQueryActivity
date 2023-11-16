db024bff319b1fd1d75c634ac91622330c65b307
Lauren <lauren.roskopf@gmail.com>
2023-11-15 19:55:09 -0600

    added to page

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 //1
+
 db.collection("teams")
   .where("Country", "==", "Spain")
   .get()
   .then((data) => {
     let docs = data.docs;
+    document.querySelector("#answers").innerHTML += `Question 1`;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
 //2
+
 db.collection("teams")
   .where("City", "==", "Madrid")
   .where("Country", "==", "Spain")
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 2`;
     let docs = data.docs;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
 
@@ -25,9 +35,13 @@ db.collection("teams")
   .where("City", "==", "Not applicable")
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 3`;
     let docs = data.docs;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
 
@@ -36,8 +50,12 @@ db.collection("teams")
   .where("Country", "!=", "Spain")
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 4`;
     let docs = data.docs;
     docs.forEach((d) => {
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
       console.log(d.data());
     });
   });
@@ -47,56 +65,74 @@ db.collection("teams")
   .where("Country", "not-in", ["Spain", "England"])
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 5`;
     let docs = data.docs;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
 
-// //6 teams in spain w more than 700 mil fans
-db.collection("teams")
-  .where("Country", "!=", "Spain")
-  .where("Fans (million)", ">", 700)
-  .get()
-  .then((data) => {
-    let docs = data.docs;
-    docs.forEach((d) => {
-
-      console.log(d.data());
-    });
-  });
-
-//7
+// // //6 teams in spain w more than 700 mil fans
 // db.collection("teams")
-//   .where("Fans (million)", ">", 500)
-//   .where("Fans (million)", "<", 600)
+//   .where("Country", "!=", "Spain")
+//   .where("Fans (million)", ">", 700)
 //   .get()
 //   .then((data) => {
+//document.querySelector("#answers").innerHTML += `Question 6`;
 //     let docs = data.docs;
 //     docs.forEach((d) => {
 //       console.log(d.data());
+// document.querySelector("#answers").innerHTML += `<p> ${
+//     d.data().Name
+//   }</p>`;
 //     });
 //   });
+
+//7
+db.collection("teams")
+  .where("Fans (million)", ">", 500)
+  .where("Fans (million)", "<", 600)
+  .get()
+  .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 7`;
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
+    });
+  });
 
 //8 ronaldo is top scorer
 db.collection("teams")
   .where("Top Scorers", "array-contains", "Ronaldo")
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 8`;
     let docs = data.docs;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
-
 
 //9
 db.collection("teams")
   .where("Top Scorers", "array-contains-any", ["Ronaldo", "Maradona", "Messi"])
   .get()
   .then((data) => {
+    document.querySelector("#answers").innerHTML += `Question 9`;
     let docs = data.docs;
     docs.forEach((d) => {
       console.log(d.data());
+      document.querySelector("#answers").innerHTML += `<p> ${
+        d.data().Name
+      }</p>`;
     });
   });
