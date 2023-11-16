aa6c5f507501f555ae943a8ba4a087963578832b
Lauren <lauren.roskopf@gmail.com>
2023-11-15 19:31:01 -0600

    update4

--- app.js
+++ app.js
@@ -52,10 +52,43 @@ db.collection("teams")
     });
   });
 
-//teams in spain w more than 700 mil fans
+// //teams in spain w more than 700 mil fans
+// db.collection("teams")
+//   .where("Country", "!=", "Spain")
+//   .where("Fans (million)", ">", 700)
+//   .get()
+//   .then((data) => {
+//     let docs = data.docs;
+//     docs.forEach((d) => {
+//       console.log(d.data());
+//     });
+//   });
+
+//
 db.collection("teams")
-  .where("Country", "!=", "Spain")
-  .where("Fans (million)", ">", 700)
+  .where("Fans (million)", ">", 500)
+  .where("Fans (million)", "<", 600)
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+//ronaldo is top scorer
+db.collection("teams")
+  .where("Top Scorers", "array-contains", "Ronaldo")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+db.collection("teams")
+  .where("Top Scorers", "array-contains-any", ["Ronaldo", "Maradona", "Messi"])
   .get()
   .then((data) => {
     let docs = data.docs;
