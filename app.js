153d3761980f119b6e9a28f5ecf865f8ab566418
Lauren <lauren.roskopf@gmail.com>
2023-11-15 19:09:01 -0600

    update

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-db.collection('teams').where('Country', "=", "Spain")
-    .get().then((data) => {
-        let docs = data.docs;
-        docs.forEach((d) => {
-            console.log(d.data());
-        });
+db.collection("teams")
+  .where("Country", "==", "Spain")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
     });
+  });
+
+db.collection("teams")
+  .where("City", "==", "Madrid")
+  .where("Country", "==", "Spain")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+db.collection("teams")
+  .where("City", "==", "Not applicable")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
