20f7939e8ee20f453d612445da756203bb3402d9
Lauren <lauren.roskopf@gmail.com>
2023-11-15 19:11:19 -0600

    update 2

--- app.js
+++ app.js
@@ -19,8 +19,41 @@ db.collection("teams")
     });
   });
 
+//national teams
 db.collection("teams")
   .where("City", "==", "Not applicable")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+//teams not in spain
+db.collection("teams")
+  .where("Country", "!=", "Spain")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+db.collection("teams")
+  .where("Country", "==", "Spain")
+  .get()
+  .then((data) => {
+    let docs = data.docs;
+    docs.forEach((d) => {
+      console.log(d.data());
+    });
+  });
+
+//not in spain or england
+db.collection("teams")
+  .where("Country", "not in", ["Spain", "England"])
   .get()
   .then((data) => {
     let docs = data.docs;
